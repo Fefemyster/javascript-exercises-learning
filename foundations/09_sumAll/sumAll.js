effacefbf51cf3b7a670d17a1d1075f716a7d7c6
Kenenn Lara <kenennlara@gmail.com>
2026-02-23 21:45:30 -0600

    Complete 09_sumAll from Foundations Course
    Create an array and add its values.

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function (num1, num2) {
+  //1. Declare an empty array for saving the generated numbers
+  let arr = [];
 
+  for (i = num1; i <= num2; i++) {
+    //2. Generate numbers
+    let generatedNumbers = i;
+
+    //3 Put numbers into our empty array
+
+    arr.push(generatedNumbers);
+  }
+  //4. Sum all the numbers inside the array
+  let result = arr.reduce((sum, current) => sum + current);
+
+  return result;
 };
 
 // Do not edit below this line
