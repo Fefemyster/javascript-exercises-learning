48744db840c9b8106b3a4bc80f8e4a9e1e1b1ffb
Kenenn Lara <kenennlara@gmail.com>
2026-02-23 19:54:43 -0600

    Complete 06_repeatString and 07_reverseString
    Solve 06 and 07 exercises from foundations course.

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
+const repeatString = function (word, times) {
+  if (times < 0) {
+    return "ERROR";
+  }
+  let string = "";
 
+  for (i = 0; i < 3; i++) {
+    string = string + word;
+  }
+
+  return string;
 };
+// repeatString('hey', 3) // returns 'heyheyhey'
 
 // Do not edit below this line
 module.exports = repeatString;
--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  const reverseString = function (string) {
+    const arr = string.split("").reverse().join("");
+    return arr;
+  };
 };
+
+reverseString("hello there");
 
 // Do not edit below this line
 module.exports = reverseString;
