ce4d3292b4e47879fd31666c97777fa412580048
Kenenn Lara <kenennlara@gmail.com>
2026-02-23 21:04:17 -0600

    Complete 08_removeFromArray from Foundations
    Solve exercise using array method .filter.

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,9 @@
-const removeFromArray = function() {
+const removeFromArray = function (items, condition) {
+  return items.filter((item) => {
+    return item !== condition;
+  });
 };
 
+let items = [1, 2, 3, 4, 5];
 // Do not edit below this line
 module.exports = removeFromArray;
