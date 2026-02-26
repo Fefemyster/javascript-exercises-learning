d992682a6335729c3e7224be7687b6f29f2fe2c1
Kenenn Lara <kenennlara@gmail.com>
2026-02-25 19:35:04 -0600

    Complete 10_leapYears
    Solve exercise ten on the foundations course. Determines if the year passed to the function is a leap or not.

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  if (year % 4 === 0 && year % 400 === 0) {
+    console.log(`Year ${year} is leap`);
+    return true;
+  } else if (year % 100 === 0) {
+    console.log(`Year ${year} is not leap`);
+    return false;
+  } else {
+    console.log(`Year ${year} is not leap`);
+    return false;
+  }
 };
 
 // Do not edit below this line
