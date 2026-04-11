910ced6bc06338754912f3fcd12c5dc3475c1559
Kenenn Lara <kenennlara@gmail.com>
2026-04-10 21:26:49 -0600

    Complete 13_palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (text) {
+  // Since we only consider letters and numbers, create a variable containing all valid characters
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz0123456789";
+  //1st. Convert text into an array
+  const cleanArray = text
+    .toLowerCase()
+    .split("")
+    .filter((character) => alphanumerical.includes(character))
+    .join("");
 
+  //2nd. Reverse string
+  const reversedArray = cleanArray.split("").reverse().join("");
+
+  //3rd. Compare original array with reverse one
+  /*
+  if (cleanArray === reversedArray) {
+    return true;
+  } else {
+    return false;
+  }
+    */
+
+  return cleanArray === reversedArray;
 };
+
+console.log(palindromes("A car, a man, a maraca."));
 
 // Do not edit below this line
 module.exports = palindromes;
