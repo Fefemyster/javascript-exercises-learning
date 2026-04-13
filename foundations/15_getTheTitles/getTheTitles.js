d05a7a5c58cd4eb49b3401a690a32dafc6e2c2b8
Kenenn Lara <kenennlara@gmail.com>
2026-04-12 21:21:34 -0600

    Complete 15_getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,21 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: "Book",
+    author: "Name",
+  },
+  {
+    title: "Book2",
+    author: "Name2",
+  },
+];
 
+const getTheTitles = function (books) {
+  //1st. Extract titles of objects
+  const bookTitle = books.map((books) => books.title);
+  return console.log(bookTitle);
 };
+
+getTheTitles(books);
 
 // Do not edit below this line
 module.exports = getTheTitles;
